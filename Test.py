9149bf1f762701a852dbf80c698c519410066000
Karan <karanshetty1441994@gmail.com>
2024-10-27 20:50:34 +0530

    commit by dev

--- Test.py
+++ Test.py
@@ -1,1 +1,18 @@
 print("Testing only")
+
+
+def printsum(a, b):
+    return a + b
+
+
+obj = printsum(4, 5)
+print(obj)
+
+
+def printeven(n):
+    if n % 2 == 0:
+        return "Its even"
+
+
+obj1 = printeven(4)
+print(obj1)
--- test2.py
+++ test2.py
@@ -1,2 +1,5 @@
 print("1")
 print("2")
+
+list1=[4,5,6]
+print(len(list1))
